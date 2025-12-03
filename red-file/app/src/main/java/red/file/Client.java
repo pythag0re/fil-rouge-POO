1274f9c05baeebd17c62c32dd69b68cee3058fa1
pythag0re <byacine69360@gmail.com>
2025-12-03 11:23:52 +0100

    initialize project

---
+++ red-file/app/src/main/java/red/file/Client.java
@@ -0,0 +1,15 @@
+package red.file;
+
+public class Client {
+    private int id;
+    private String FirstName;
+    private String name;
+    private String email;
+
+    public Client(int id, String firstName, String name, String email) {
+        this.id = id;
+        this.FirstName = firstName;
+        this.name = name;
+        this.email = email;
+    }
+}
